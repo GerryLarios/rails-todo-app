f8aa662e5fe7656ae2bb48937f3b65adc2aafc00
Gerry <gerardo.larios69@gmail.com>
2021-02-05 00:13:59 -0600

    [ADD] seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,18 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.find(1)
+
+music_list = List.create!(name: 'Recomended Music Bands', user: user)
+movie_list = List.create!(name: 'Movies to see', user: user)
+book_list = List.create!(name: 'Books I want to read', user: user)
+
+x = 0
+
+while x < 20
+  music_task = Task.create!(title: Faker::Music.unique.band, list: music_list)
+  puts "MUSIC TASK CREATED #{music_task.title}"
+  movie_task = Task.create!(title: Faker::Movie.unique.title, list: movie_list)
+  puts "MOVIE TASK CREATED #{movie_task.title}"
+  book_task = Task.create!(title: Faker::Game.unique.title, list: book_list)
+  puts "BOOK TASK CREATED #{book_task.title}"
+  puts '====================================='
+  x += 1
+end
