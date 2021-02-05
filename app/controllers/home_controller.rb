35e9a8cd703082ae3e093522ea150132a613893f
Gerry <gerardo.larios69@gmail.com>
2021-02-04 22:13:50 -0600

    [ADD] authenticate first

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
+# HomeController
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+
   def index
     redirect_to lists_path if user_signed_in?
   end
